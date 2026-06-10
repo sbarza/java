b0c3609487c4839a38c9792e47f8f640cade1e65
sbarza <sergiobarza@gmail.com>
2026-06-10 00:30:50 -0300

    Creating Your First Generic Class: From Basics to Implementation: Attempt to Use Interface in order to Handle Different Types

--- 12-generics/src/Main.java
+++ 12-generics/src/Main.java
@@ -1,12 +1,19 @@
 
-record BaseballPlayer(String name, String position) {}
+interface Player {}
+
+record BaseballPlayer(String name, String position) implements Player {}
+record FootballPlayer(String name, String position) implements Player {}
 
 public class Main {
 
     public static void main(String[] args) {
 
-        BaseballTeam phillies = new BaseballTeam("Philadelphia Phillies");
-        BaseballTeam astros = new BaseballTeam("Huston Astros");
+        BaseballTeam phillies1 = new BaseballTeam("Philadelphia Phillies");
+        BaseballTeam astros1 = new BaseballTeam("Huston Astros");
+        scoreResult(phillies1, 3, astros1, 5);
+
+        SportsTeam phillies = new SportsTeam("Philadelphia Phillies");
+        SportsTeam astros = new SportsTeam("Huston Astros");
         scoreResult(phillies, 3, astros, 5);
 
         var harper = new BaseballPlayer("B Harper", "Right Fielder");
@@ -14,10 +21,28 @@ public class Main {
         phillies.addTeamMember(harper);
         phillies.addTeamMember(marsh);
         phillies.listTeamMembers();
+
+        SportsTeam afc = new SportsTeam("Adelaide Crows");
+        var tex = new FootballPlayer("Tex Walker", "Centre half forward");
+        afc.addTeamMember(tex);
+        afc.listTeamMembers();
+
+        var guthrie = new BaseballPlayer("D Guthrie", "Center Fielder");
+        afc.addTeamMember(guthrie);
+        afc.listTeamMembers();
+
     }
 
     public static void scoreResult(BaseballTeam team1, int t1_score,
                                    BaseballTeam team2, int t2_score) {
+
+        String message = team1.setScore(t1_score, t2_score);
+        team2.setScore(t2_score, t1_score);
+        System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+
+    public static void scoreResult(SportsTeam team1, int t1_score,
+                                   SportsTeam team2, int t2_score) {
 
         String message = team1.setScore(t1_score, t2_score);
         team2.setScore(t2_score, t1_score);
