87030e40f3f77cd18b56ab030c493fcc8c093924
sbarza <sergiobarza@gmail.com>
2026-06-10 01:43:24 -0300

    Creating Your First Generic Class: From Basics to Implementation: Team Generic Class - Usage of Type Parameters - Defining Upper Bounds for Parameterized Type T

--- 12-generics/src/Main.java
+++ 12-generics/src/Main.java
@@ -1,12 +1,17 @@
 
-interface Player {}
+interface Player {
+    String name();
+}
 
 record BaseballPlayer(String name, String position) implements Player {}
 record FootballPlayer(String name, String position) implements Player {}
+record VolleyballPlayer(String name, String position) implements Player {}
 
 public class Main {
 
     public static void main(String[] args) {
+
+        var philly = new Affiliation("City", "Philadelphia, PA", "US");
 
         BaseballTeam phillies1 = new BaseballTeam("Philadelphia Phillies");
         BaseballTeam astros1 = new BaseballTeam("Huston Astros");
@@ -16,8 +21,8 @@ public class Main {
         SportsTeam astros2 = new SportsTeam("Huston Astros");
         scoreResult(phillies2, 3, astros2, 5);
 
-        Team<BaseballPlayer> phillies = new Team<>("Philadelphia Phillies");
-        Team<BaseballPlayer> astros = new Team<>("Huston Astros");
+        Team<BaseballPlayer, Affiliation> phillies = new Team<>("Philadelphia Phillies", philly);
+        Team<BaseballPlayer, Affiliation> astros = new Team<>("Huston Astros");
         scoreResult(phillies, 3, astros, 5);
 
         var harper = new BaseballPlayer("B Harper", "Right Fielder");
@@ -29,23 +34,24 @@ public class Main {
         phillies.listTeamMembers();
 
         SportsTeam afc1 = new SportsTeam("Adelaide Crows");
-        Team<FootballPlayer> afc = new Team<>("Adelaide Crows");
+        Team<FootballPlayer, String> afc = new Team<>("Adelaide Crows",
+                "City of Adelaide, South Australia, in AU");
         var tex = new FootballPlayer("Tex Walker", "Centre half forward");
         afc.addTeamMember(tex);
         var rory = new FootballPlayer("Rory Laird", "Midfield");
         afc.addTeamMember(rory);
         afc.listTeamMembers();
 
-        Team<String> adelaide = new Team<>("Adelaide Storm");
-        adelaide.addTeamMember("N Roberts");
+        Team<VolleyballPlayer, Affiliation> adelaide = new Team<>("Adelaide Storm");
+        adelaide.addTeamMember(new VolleyballPlayer("N Roberts", "Setter"));
         adelaide.listTeamMembers();
 
-        var canberra = new Team<>("Canberra Heat");
-        canberra.addTeamMember("B Black");
+        var canberra = new Team<VolleyballPlayer, Affiliation>("Canberra Heat");
+        canberra.addTeamMember(new VolleyballPlayer("B Black", "Opposite"));
         canberra.listTeamMembers();
         scoreResult(canberra, 0, adelaide, 1);
 
-        Team<Integer> melbourneVB = new Team<>("Melbourne Vipers");
+//        Team<Integer> melbourneVB = new Team<>("Melbourne Vipers");
     }
 
     public static void scoreResult(BaseballTeam team1, int t1_score,
